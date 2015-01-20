58694df1043d377dc683d4745d9a58eefc7e06f9
zck921031 <zck921031@qq.com>
2015-01-20 18:22:35 +0800

    添加直方图特征方法
    添加直方图特征方法

--- Image Annotation/matlab/demo1.m
+++ Image Annotation/matlab/demo1.m
@@ -16,10 +16,9 @@ function demo1()
     end
     
     %figure(2);
-    %imshow( rgb2gray(suns{9}) );
-    
+    %imshow( rgb2gray(suns{9}) );    
     [sizeA, sizeB, sizeC] = size( suns{1} );
-    img = zeros( sizeA, sizeB, D );
+    img = uint8( zeros( sizeA, sizeB, D ) );
     for i=1:D
         tmp = rgb2gray(suns{i});
         img(:,:,i) = tmp(:,:);
@@ -28,19 +27,75 @@ function demo1()
     figure(3);
     for i=1:D
         subplot(2,5,i);
-        imshow( uint8( img(:,:,i) ) );
+        imshow( img(:,:,i) );
     end
     
     figure(4);
-    for i=1:D        
-        subplot(2,5,i);
-        res = hist_feature( uint8(img(:,:,i)) );
-        bar(res);        
-    end
+    show_hist_feature( img );
+    
+    figure(5);
+    res = hist_feature_1(img);
+    bar(res);
+    
+    figure(6);
+    res = hist_feature_2(img, 8);
+    bar(res);
+    
+    figure(7);
+    res = hist_feature_3(img);
+    bar(res);
     
 end
 
+%画出直方图看看~
+function show_hist_feature(image)
+    [A, B, D] = size(image);
+    for i=1:D
+        subplot(2,5,i);
+        tmp = imhist( image(:,:,i) );
+        bar(tmp);
+    end
+end
 
-function [ret]=hist_feature(image)
-    ret = imhist(image);
+%颜色向量，统计每个波段图片像素值的和，可以看成统计每个波段的能量大小。即颜色一阶矩(mean)。
+function ret = hist_feature_1(image)
+    [A, B, D] = size(image);
+    ret = zeros(1,D);
+    for i=1:D
+        ret(i) = sum( sum( image(:,:,i) ) );
+    end
+    %ret = ret./norm(ret,2);
+    ret = ret./(A*B);
+end
+
+%D维-颜色直方图，量化级数为N
+function ret = hist_feature_2(image, N)
+    [A, B, D] = size(image);
+    ret = zeros(1,N*D);
+    for i=1:D
+        tmp = imhist( image(:,:,i), N );
+        for j=1:N
+            ret( (i-1)*N + j ) = tmp(j);
+        end
+    end
+    ret = ret./(A*B);
+end
+
+%颜色矩(mean, viarance, skewness)。
+function ret = hist_feature_3(image)
+    [A, B, D] = size(image);
+    mu = zeros(1,D);
+    sigma = zeros(1,D);
+    s = zeros(1,D);
+    for i=1:D
+        mu(i) = sum( sum( image(:,:,i) ) )/(A*B);
+    end
+    gg = ( double( image(:,:,i) ) - mu(i) );
+    for i=1:D
+        sigma(i) = ( sum(sum( ( double( image(:,:,i) ) - mu(i) ).^2 ))/(A*B) )^(1/2);
+    end
+    for i=1:D
+        s(i) = ( sum(sum( abs( double( image(:,:,i) ) - mu(i) ).^3 ) )/(A*B) )^(1/3);
+    end
+    ret = [ mu, sigma, s ];
 end
